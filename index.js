c700f26e395c1c74b504cd95e58e939fec7e62f0
johannamag <69107842+johannamag@users.noreply.github.com>
2021-05-24 19:01:58 +0200

    added media queries nav and toggle function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const menuIcon = document.querySelector(".menu-icons");
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
 
+menuIcon.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  hamburger.classList.toggle("open");
+  close.classList.toggle("open");
+});
